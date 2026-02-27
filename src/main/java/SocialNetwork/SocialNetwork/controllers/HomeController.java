f681aba4651a70d9c19e8fd441392d208da1b172
DungMD <noname12102002@gmail.com>
2026-02-27 16:21:47 +0700

    chore: update api check to trigger send request to Render

--- src/main/java/SocialNetwork/SocialNetwork/controllers/HomeController.java
+++ src/main/java/SocialNetwork/SocialNetwork/controllers/HomeController.java
@@ -1,17 +1,22 @@
 package SocialNetwork.SocialNetwork.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @GetMapping("/login_google")
     public String index() {
         return "Welcome! <a href='/oauth2/authorization/google'>Login with Google</a>";
     }
 
-    @GetMapping("/home")
-    public String home() {
-        return "Hello Dung Mai";
+    @GetMapping("/check")
+    public String check() {
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        return "OK";
     }
 }
